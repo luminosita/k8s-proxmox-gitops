5a4a5f0c3f7539f8e3fb2bf82bd425999f6c9af6
luminosita <milosh@emisia.net>
2025-04-16 17:18:09 +0200

    cluster name, bgp/proxmox version

--- tf/providers.tf
+++ tf/providers.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.74.1"
+      version = "0.75"
     }    
     kustomization = {
       source  = "kbst/kustomization"
